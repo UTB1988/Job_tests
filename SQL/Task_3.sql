8e3368dc5578dc7eb82c1328d5d3accd9c1ced61
Kostenko Pavel <kp11021988@gmail.com>
2022-04-30 16:43:19 +0300

    Improved readability

--- SQL/Task_3.sql
+++ SQL/Task_3.sql
@@ -1,13 +1,16 @@
 -- MS SQL SERVER
 
-/* --- --- Подготовка / Preperation --- --- */
+/*==================================================================================================================================================*/
+/* --- --- Подготовка исходных данных / Preperation of source data --- --- */
+/*==================================================================================================================================================*/
 	
-	/* --- Таблица / Table - 1 - #dataset --- */
+	/* --- #dataset --- */
+		
 		IF OBJECT_ID('tempdb.dbo.#dataset', 'U') IS NOT null
 		DROP TABLE #dataset;
 		
 		CREATE TABLE #dataset (
-			key_field VARCHAR(34)
+			  key_field VARCHAR(34)
 			, field_name VARCHAR(34)
 			, field_value VARCHAR(34));
 		
@@ -19,30 +22,35 @@
 			('id_003', 'Тур2', '2'),
 			('id_002', 'Тур3', '-9');
 
+/*==================================================================================================================================================*/
 /* --- --- Задание / Task --- --- */
+/*==================================================================================================================================================*/
 	
-	-- Используя таблицу 1, создать и заполнить данными результирующую таблицу.
+	-- Используя таблицу #dataset, создать и заполнить данными результирующую таблицу.
 		
 	/* --- Вариация решения / Solution variation - 1 --- */
+		
 		SELECT
-			DISTINCT(t.key_field)
+			  DISTINCT(t.key_field)
 			, (SELECT t1.field_value FROM #dataset t1 WHERE t1.key_field = t.key_field AND t1.field_name = 'Тур1') AS "Тур1"
 			, (SELECT t2.field_value FROM #dataset t2 WHERE t2.key_field = t.key_field AND t2.field_name = 'Тур2') AS "Тур2"
 			, (SELECT t3.field_value FROM #dataset t3 WHERE t3.key_field = t.key_field AND t3.field_name = 'Тур3') AS "Тур3"
 		FROM #dataset t;
 	
 	/* --- Вариация решения / Solution variation - 2 --- */
+		
 		SELECT DISTINCT
-			t.key_field
+			  t.key_field
 			, t1.field_value AS "Тур1"
 			, t2.field_value AS "Тур2"
 			, t3.field_value AS "Тур3"
 		FROM #dataset t
-		LEFT JOIN #dataset t1 ON t1.key_field = t.key_field AND t1.field_name = 'Тур1'
-		LEFT JOIN #dataset t2 ON t2.key_field = t.key_field AND t2.field_name = 'Тур2'
-		LEFT JOIN #dataset t3 ON t3.key_field = t.key_field AND t3.field_name = 'Тур3';
+			LEFT JOIN #dataset t1 ON t1.key_field = t.key_field AND t1.field_name = 'Тур1'
+			LEFT JOIN #dataset t2 ON t2.key_field = t.key_field AND t2.field_name = 'Тур2'
+			LEFT JOIN #dataset t3 ON t3.key_field = t.key_field AND t3.field_name = 'Тур3';
 
+/*==================================================================================================================================================*/
 /* --- --- Удалить временные таблицы / Delete temporary tables --- --- */
+/*==================================================================================================================================================*/
 	
-	IF OBJECT_ID('tempdb.dbo.#dataset', 'U') IS NOT null
-	DROP TABLE #dataset;
+	IF OBJECT_ID('tempdb.dbo.#dataset', 'U') IS NOT null DROP TABLE #dataset;
